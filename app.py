50f2c48af9c36e04d4e4815227776cbbe7d33b0c
m_khandaker <m_khandaker@eaglys.co.jp>
2022-11-08 22:50:48 +0900

    Add dockerfile

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = FastAPI(
     title="k8s-demo",
     description="k8s Demo using FastAPI",
     version="0.1.0",
-    contact={"name":"k8s-demo", "url": "localhost:5000"},
+    contact={"name": "k8s-demo", "url": "localhost:5000"},
     debug=True,
 )
 
@@ -17,10 +17,20 @@ app = FastAPI(
 def code_format():
     os.system("sh code_format.sh")
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-#start the server
-def start_server():
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+def start():
     uvicorn.run(app, host="127.0.0.1", port=5000)
+
+
+# if __name__ == "__main__":
+#     start()
